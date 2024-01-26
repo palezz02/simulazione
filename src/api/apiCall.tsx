1e6657ad971fe0013824f26734911e79abb23ee2
palezz <mail@example.com>
2024-01-26 11:33:52 +0100

    Parte 2

--- src/api/apiCall.tsx
+++ src/api/apiCall.tsx
@@ -1,2 +1,20 @@
 import axios from "axios";
 const api_key = "c3804e9a";
+
+export async function getMovies() {
+  try {
+    const response = await axios.get(`http://www.omdbapi.com/?apikey=${api_key}`);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getMovie() {
+  try {
+    const response = await axios.get(`http://www.omdbapi.com/?apikey=${api_key}&t=s`);
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
